03a3ed88505037d2c541d11ecdee3d76a4a3078c
Raymond LeClair <raymond.leclair@gmail.com>
2014-04-18 22:34:37 -0400

    Removed unneeded files. Commit processor code and processed data.

--- R/Answers.R
+++
@@ -1,7 +0,0 @@
-## ~1~ dropbox.dir, "*.gazedata", full.names=TRUE
-## ~2~ ".gazedata", ".RData", basename(inp.file)
-## ~3~ /
-## ~4~ inp.file
-## ~5~ data.full, TRUE, select=c("Subject", "Session", "ID", "TETTime", "RTTime", "CursorX", "CursorY")
-## ~6~ out.file
-## ~7~ out.file
--- R/Hints.R
+++
@@ -1,7 +0,0 @@
-## /
-## out.file
-## out.file
-## data.full, TRUE, select=c("Subject", "Session", "ID", "TETTime", "RTTime", "CursorX", "CursorY")
-## ".gazedata", ".RData", basename(inp.file)
-## inp.file
-## dropbox.dir, "*.gazedata", full.names=TRUE
