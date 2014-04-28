3e7ad8fa3af080ba1da77956f3341c6b98875159
Raymond LeClair <raymond.leclair@gmail.com>
2014-04-28 13:08:23 -0400

    Added SOURCEME.R

---
+++ R/SOURCEME.R
@@ -0,0 +1,15 @@
+### SOURCEME.R
+###
+### TODO: Complete.
+### 
+### Copyright (c) 2014 Jessica E. and Raymond A. LeClair
+### 
+### This program can be redistributed and/or modified under the terms
+### of the MIT License as published by the Open Source Initiative.
+### 
+### See the LICENSE file or http://opensource.org/licenses/MIT.
+
+source('./R/EyeTrackingAnalysis.R')
+
+data <- selectData()
+data <- assignTreatment(data)
